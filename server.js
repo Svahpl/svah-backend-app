d1ac0e065fe889973633535fbbca9594ec57d762
Viral Vaghela <viralvaghela8577@gmail.com>
2025-05-28 11:42:18 +0530

    chore: remove unnecessary comment line in server.js

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors(corsOptions));
+// 
 
 // ========================== IMPORTANT: Webhook Route =========================== //
 
