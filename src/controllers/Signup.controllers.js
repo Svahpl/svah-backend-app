f93920854b568b839e114b991a612ad0ba1d2789
divyeshmoraniya <hdivyesh62@gmail.com>
2025-06-16 18:37:20 +0530

    Update Signup.controllers.js

--- src/controllers/Signup.controllers.js
+++ src/controllers/Signup.controllers.js
@@ -56,6 +56,19 @@ export const getAllUser = async (req, res) => {
     }
 };
 
+export const getAUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+        console.log(user);
+        if (!user) {
+            return res.status(400).json({ msg: "User not found" });
+        }
+        return res.status(200).json({ message: "User found", user });
+    } catch (error) {
+        console.error("Error in getUser:", error);
+        return res.status(500).json({ msg: "Internal server error", error });
+    }
+  };
 
 export const getAdmin = async (req, res) => {
     try {
