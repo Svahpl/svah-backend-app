a00d4ed99683314ec905b92c51866cf50705fa7f
divyeshmoraniya <hdivyesh62@gmail.com>
2025-06-18 17:30:24 +0530

    Update form.router.js

--- src/router/form.router.js
+++ src/router/form.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { MessegeSubmit, SalseMessegeSubmit, getrequirement, getsalse } from "../controllers/formsubmission.controllers.js"
 export const formRouter = new Router();
+import {AdminVerify} from "../middlewares/Admin.middlewares.js"
 
 formRouter.route("/requirementform").post(MessegeSubmit);
 formRouter.route("/salseform").post(SalseMessegeSubmit);
-formRouter.route("/getrequirement").get(getrequirement);
-formRouter.route("/getsalse").get(getsalse);
+formRouter.route("/getrequirement").get(AdminVerify,getrequirement);
+formRouter.route("/getsalse").get(AdminVerify, getsalse);
