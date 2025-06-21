81b87da1ac913fb6467be69f823fd9f762119078
divyeshmoraniya <hdivyesh62@gmail.com>
2025-06-21 13:51:31 +0530

    Update whishlist.router.js

--- src/router/whishlist.router.js
+++ src/router/whishlist.router.js
@@ -1,8 +1,9 @@
 import Router from "express";
-import { addItemToWishList, deletWishListitem, getWishListItem } from "../controllers/whishlist.controllers.js";
+import { addItemToWishList, deletWishListitem, getWishListItem, moveWishlistToCart } from "../controllers/whishlist.controllers.js";
 
 export const WishlistRouter = new Router();
 
 WishlistRouter.route("/add-to-wishlist/:userId/:productId").post(addItemToWishList);
 WishlistRouter.route("/delete-to-wishlist/:userId/:productId").delete(deletWishListitem);
 WishlistRouter.route("/getwhishlistItem/:userId").get(getWishListItem);
+WishlistRouter.route("/movetocart").post(moveWishlistToCart)
