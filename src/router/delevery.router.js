3641c761970611306bdb9407c41a9cb320dfc152
divyeshmoraniya <hdivyesh62@gmail.com>
2025-06-26 18:10:08 +0530

    Update delevery.router.js

--- src/router/delevery.router.js
+++ src/router/delevery.router.js
@@ -1,7 +1,7 @@
 import Router from "express";
 import { updatecharge , getcharge } from "../controllers/deliveryChrage.controllers.js";
-
+import { AdminVerify } from "../middlewares/Admin.middlewares.js";
 export const deliveryRouter = Router();
 
-deliveryRouter.route("/update-deliverycharge").put(updatecharge);
-deliveryRouter.route("/getcharge").get(getcharge);
+deliveryRouter.route("/update-deliverycharge").put(AdminVerify,updatecharge);
+deliveryRouter.route("/getcharge").get(AdminVerify,getcharge);
